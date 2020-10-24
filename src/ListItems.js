1e0208ee6cca3b677879f2f2998f40fa5f3d41bc
swathinath <swathinath.v@gmail.com>
2020-10-25 01:51:18 +0900

    Add flipmove animations

--- src/ListItems.js
+++ src/ListItems.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './ListItems.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Flipmove from 'react-flip-move';
 
 function ListItems(props){
     const items = props.items;
@@ -27,7 +28,11 @@ function ListItems(props){
             </div>
         })
     return(
-        <div>{listItems}</div>
+        <div>
+            <Flipmove duration={300} easing="ease-in-out">
+            {listItems}
+            </Flipmove>
+        </div>
     )
 }
 export default ListItems;
