a3e73a807626787694b5292ebe886b359d01e659
swathinath <swathinath.v@gmail.com>
2020-10-24 23:45:57 +0900

    Handle text inputs

--- src/App.js
+++ src/App.js
@@ -3,12 +3,50 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component{
+  constructor(props){
+    super(props);
+      this.state={
+        items:[],
+        currentItem:{
+          text:'',
+          key:''
+      }
+    }
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+  }
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        text: e.target.value,
+        key:Date.now()
+      }
+    })
+  }
+  addItem(e){
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    console.log(newItem);
+    if(newItem.text!==""){
+      const newItems=[...this.state.items, newItems];
+      this.setState({
+        items:newItems,
+        currentItem:{
+          text:'',
+          key:''
+        }
+      })
+    }
+  }
   render(){
     return (
       <div className="App">
         <header>
-          <form id="to-do-form">
-            <input type="text" placeholder="Enter Text"/>
+          <form id="to-do-form" onSubmit={this.addItem}>
+            <input type="text" placeholder="Enter Text"
+            value={this.state.currentItem.text}
+            onChange={this.handleInput}
+            />
             <button type="submit">Add</button>
           </form>
       </header>
